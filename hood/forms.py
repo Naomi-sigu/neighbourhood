836663a86824f64f214c1bf6f2b91f507f66b43d
naomi <siguneema@gmail.com>
2019-10-26 20:29:45 +0300

    create the update profile form and add the fields required

---
+++ hood/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Profile
+
+class UpdateProfile(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ['profile_pic','bio']
