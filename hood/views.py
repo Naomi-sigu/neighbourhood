191a067cf351139de242ff9af3adbe66607593ca
naomi <siguneema@gmail.com>
2019-10-26 13:59:37 +0300

    add a route for the users to view the home page

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def welcome(request):
+    current_user = request.user
+    return render (request, 'home.html')
+
+@login_required
+def profile(request):
+  current_user = request.user
+  return render(request,'profile.html') 
+ 
