9a2880007912b40890c4f94ec43d137f657270a6
naomi <siguneema@gmail.com>
2019-10-26 20:17:16 +0300

    create the neighbourhood model to hold the stracture and info

--- hood/models.py
+++ hood/models.py
@@ -5,6 +5,22 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
+class Neighbourhood(models.Model):
+    neighbourhood= models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
+
+        
+
+
 class Profile(models.Model):
   profile_pic = models.ImageField(default='../static/images/default.jpeg',upload_to='media/')
   bio = models.TextField()
@@ -18,3 +34,24 @@ class Profile(models.Model):
   @receiver(post_save,sender = User)
   def save_profile(sender,instance,**kwargs):
     instance.profile.save()
+
+class Bussiness(models.Model):
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='media/')
+    bussiness_description = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE, blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    address = models.TextField(max_length=60)
+    
+
+    def save_bussiness(self):
+        self.save()
+    
+    @classmethod
+    def get_all_bussiness(cls):
+        bussiness= Bussiness.objects.all()
+        return bussiness
+
+
+
+        
