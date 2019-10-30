bacc5056419f8068386ee33d8b7c5c6265c622d7
naomi <siguneema@gmail.com>
2019-10-30 15:57:04 +0300

    query the businesses to be viewed by the users

--- hood/views.py
+++ hood/views.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .forms import *
 from django.contrib import messages
-from .models import Profile , Neighbourhood
+from .models import *
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -29,7 +30,7 @@ def update_profile(request):
             profile.user = current_user
             profile.save()
 
-        return redirect('/')
+        return redirect('/profile')
 
     elif Profile.objects.get(user=current_user):
         profile = Profile.objects.get(user=current_user)
@@ -42,7 +43,30 @@ def update_profile(request):
 @login_required(login_url='/accounts/login/')
 def businesses(request):
     current_user=request.user
-    profile=Profile.objects.get(user=current_user)
-    businesses = Business.objects.filter(neighbourhood=profile.neighbourhood)
+    businesses = Business.objects.all()
+    
 
     return render(request,'businesses.html',{"businesses":businesses}) 
+
+
+@login_required(login_url='/accounts/login/')
+def new_business(request):
+    current_user=request.user
+    profile =Profile.objects.get(user=current_user)
+
+    if request.method=="POST":
+        form =BusinessForm(request.POST,request.FILES)
+        if form.is_valid():
+            business = form.save(commit = False)
+            business.owner = current_user
+            print('*******************************************************************************************')
+            print(profile.neighbourhood)
+            business.neighbourhood = profile.neighbourhood
+            business.save()
+
+        return HttpResponseRedirect('/businesses')
+
+    else:
+        form = BusinessForm()
+
+    return render(request,'newbusiness.html',{"form":form})
