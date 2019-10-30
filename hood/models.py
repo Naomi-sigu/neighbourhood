c7ba1dd652e2255fca6b96451b6f8d49f68f460a
naomi <siguneema@gmail.com>
2019-10-30 10:51:00 +0300

    add neighbourhood as a foreign key to the bussines model

--- hood/models.py
+++ hood/models.py
@@ -6,14 +6,19 @@ from django.db.models.signals import post_save
 # Create your models here.
 
 class Neighbourhood(models.Model):
-    neighbourhood= models.CharField(max_length=100)
-    image = models.ImageField(upload_to='media/')
+    neighbourhood = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/',blank=True)
 
     def __str__(self):
         return self.neighbourhood
 
     def save_neighbourhood(self):
         self.save()
+
+    @classmethod
+    def get_all_hoods(cls):
+        hoods = Neighbourhood.objects.all()
+        return hoods
 
     @classmethod
     def delete_neighbourhood(cls,neighbourhood):
@@ -23,7 +28,11 @@ class Neighbourhood(models.Model):
 class Profile(models.Model):
   profile_pic = models.ImageField(default='../static/images/default.jpeg',upload_to='media/')
   bio = models.TextField()
-  user = models.OneToOneField(User,on_delete = models.CASCADE)
+  neighbourhood = models.ForeignKey(Neighbourhood,default='...')
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
   
   @receiver(post_save , sender = User)
   def create_profile(instance,sender,created,**kwargs):
@@ -34,21 +43,17 @@ class Profile(models.Model):
   def save_profile(sender,instance,**kwargs):
     instance.profile.save()
 
-class Bussiness(models.Model):
-    title = models.CharField(max_length=30)
-    bussiness_description = models.TextField()
-    user = models.ForeignKey(User,on_delete=models.CASCADE, blank=True)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    address = models.TextField(max_length=60)
-    
+class Business(models.Model):
+    name =models.CharField(max_length=100)
+    description = models.TextField()
+    neighbourhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    email = models.EmailField()
+    address =models.CharField(max_length=100)
+    contact = models.IntegerField()
 
-    def save_bussiness(self):
-        self.save()
-    
-    @classmethod
-    def get_all_bussiness(cls):
-        bussiness= Bussiness.objects.all()
-        return bussiness
+    def __str__(self):
+        return self.name
 
 
 
