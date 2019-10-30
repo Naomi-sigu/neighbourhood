05a80dda9fadce415964bdd2a3fba77434b2e9de
naomi <siguneema@gmail.com>
2019-10-30 10:55:50 +0300

    add the update_profile to be routed in the pages

--- hood/urls.py
+++ hood/urls.py
@@ -11,3 +11,6 @@ urlpatterns=[
     url('profile/',views.profile,name='profile'),
     url('update/',views.update_profile,name='update_profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
