22156a85b35753ded5448e21295ef2f1b7ee12fe
naomi <siguneema@gmail.com>
2019-10-30 15:44:02 +0300

    create a add business form for the ones who wants to add a business

--- hood/forms.py
+++ hood/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import *
 
 class UpdateProfile(forms.ModelForm):
   class Meta:
     model = Profile
     fields = ['profile_pic','bio']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model=Business
+        exclude=['owner','neighbourhood']
