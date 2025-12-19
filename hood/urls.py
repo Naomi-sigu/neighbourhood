02fda3978f828058df41b7d948f6e5b91dde942a
Naomi Sigu <siguneema@gmail.com>
2025-12-19 11:02:22 +0300

    upgrade django for deployment

--- hood/urls.py
+++ hood/urls.py
@@ -1,20 +1,20 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 
 # from django.urls import path, include
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('profile/',views.profile,name='profile'),
-    url('update/',views.update_profile,name='update_profile'),
-    url(r'^businesses',views.businesses, name='businesses'),
-    url(r'^new/business$',views.new_business, name='new_business'),
-    url(r'^search/', views.search_businesses, name='search_results'),
-    url(r'^post/$', views.post, name='post'),
-    url(r'^post/new/$', views.new_post, name='new_post'),
+    re_path('^$',views.welcome,name = 'welcome'),
+    re_path('profile/',views.profile,name='profile'),
+    re_path('update/',views.update_profile,name='update_profile'),
+    re_path(r'^businesses',views.businesses, name='businesses'),
+    re_path(r'^new/business$',views.new_business, name='new_business'),
+    re_path(r'^search/', views.search_businesses, name='search_results'),
+    re_path(r'^post/$', views.post, name='post'),
+    re_path(r'^post/new/$', views.new_post, name='new_post'),
 ]
 
 if settings.DEBUG:
